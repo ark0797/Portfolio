288d33e0c714c20e9580f965554b19071d0f987e
ark0797 <n0797@yandex.ru>
2023-08-07 15:06:07 +0300

    add task Duobly Linked List with struct inside

---
+++ DataStructures_Balakirev_Stepik/DoublyLinkedList/DoublyLinkedListImplementation/DoublyLinkedListImplementation.cpp
@@ -0,0 +1,204 @@
+﻿#include <iostream>
+using namespace std;
+
+// реализуем двусвязный список руками через ООП 
+
+class Node { // один элемент двусвязного списка 
+public:
+    double data;
+    Node* prev;
+    Node* next;
+public:
+    Node(double data) {
+        this->data = data;
+        this->prev = this->next = NULL;
+    }
+};
+
+class DoublyLinkedList { // или просто LinkedList
+public:
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() { // конструктор класса 
+        head = tail = NULL;
+    }
+
+    ~DoublyLinkedList() { // самостоятельно освобождаем память 
+        while (head != NULL) {
+            pop_front();
+        }
+    }
+
+    Node* push_front(double data) { // добавление в начало 
+        Node* ptr = new Node(data);
+        // если даже head NULL, то ок 
+        ptr->next = head;
+        if (head != NULL) {
+            head->prev = ptr;
+        }
+        if (tail == NULL) { // список изначально был пустой 
+            tail = ptr;
+        }
+        head = ptr;
+        return ptr;
+    }
+
+    Node* push_back(double data) { // добавление в конец 
+        Node* ptr = new Node(data);
+        // если даже head NULL, то ок 
+        ptr->prev = tail;
+        if (tail != NULL) {
+            tail->next = ptr;
+        }
+        if (head == NULL) { // список изначально был пустой 
+            head = ptr;
+        }
+        tail = ptr;
+        return ptr;
+    }
+
+    void pop_front() { // удаление из начала
+        if (head == NULL) { // удалять нечего 
+            return;
+        }
+        Node* ptr = head->next;
+        if (ptr == NULL) { // элемент в списке только 1
+            delete head;
+            head = tail = NULL;
+            return;
+        }
+        ptr->prev = NULL;
+        delete head;
+        head = ptr;
+    }
+
+    void pop_back() { // удаление из конца
+        if (head == NULL) { // удалять нечего 
+            return;
+        }
+        Node* ptr = tail->prev;
+        if (ptr == NULL) { // элемент в списке только 1
+            delete tail;
+            head = tail = NULL;
+            return;
+        }
+        ptr->next = NULL;
+        delete tail;
+        tail = ptr;
+    }
+
+    Node* getAt(int k) { // получение k-ого элемента 
+        if (k < 0) return NULL;
+        Node* ptr = head;
+        int n = 0;
+        while (ptr && n != k) {
+            ptr = ptr->next;
+            n++;
+        }
+        return ptr;
+    }
+
+    // перегружаем оператор квадратных скобок 
+    Node* operator [] (int index) {
+        return getAt(index);
+    }
+
+    Node* insert(int k, double data) { // вставка элемента после k-ого
+        Node* left = getAt(k);
+        if (left == NULL) { // не мж вставить после такой позиции
+            return NULL;
+        }
+        Node* right = left->next;
+        if (right == NULL) { // делаем вставку в конец 
+            return push_back(data);
+        }
+        // здесь реализуем нормальную вставку между элементами 
+        Node* ptr = new Node(data);
+        left->next = ptr;
+        ptr->prev = left;
+        ptr->next = right;
+        right->prev = ptr;
+        return ptr;
+    }
+
+    void erase(int k) { // удаление k-ого элемента 
+        Node* ptr = getAt(k);
+        if (ptr == NULL) return; // нет удаляемого элемента 
+        if (ptr->prev == NULL) { // это удаление 1-ого элемента 
+            pop_front();
+            return;
+        }
+        if (ptr->next == NULL) { // это удаление конечного элемента 
+            pop_back();
+            return;
+        }
+        // здесь обычная ситуация удаления
+        Node* left = ptr->prev;
+        Node* right = ptr->next;
+        delete ptr;
+        left->next = right;
+        right->prev = left;
+    }
+
+    void print_dlst() { // движение вправо 
+        for (Node* ptr = head; ptr != NULL; ptr = ptr->next) {
+            cout << ptr->data << ' ';
+        }
+        cout << endl;
+    }
+
+    void revprint_dlst() { // движение влево 
+        for (Node* ptr = tail; ptr != NULL; ptr = ptr->prev) {
+            cout << ptr->data << ' ';
+        }
+        cout << endl;
+    }
+};
+int main()
+{
+    DoublyLinkedList dlst;
+    dlst.push_back(1.56);
+    dlst.push_back(3.56);
+    dlst.push_back(4.56);
+    dlst.push_back(7.56);
+    dlst.push_back(2.56);
+    dlst.push_back(8.56);
+    dlst.push_back(19.56);
+    dlst.push_back(47.56);
+    dlst.print_dlst();
+    cout << dlst[3]->data << ' ' << dlst[0]->data << endl;
+    //cout << dlst[10]->data << endl;
+    dlst.erase(4);
+    cout << "printing" << endl;
+    dlst.print_dlst();
+    dlst.revprint_dlst();
+    cout << endl;
+    Node* node = dlst.getAt(0);
+    if (node != NULL) {
+        cout << node->data << endl;
+    }
+    else {
+        cout << NULL << endl;
+    }
+    dlst.insert(0, 78.90);
+    dlst.pop_front();
+    dlst.pop_back();
+    dlst.print_dlst();
+
+    cout << "here" << endl;
+    DoublyLinkedList dlst1;
+    dlst1.push_front(4.56);
+    dlst1.pop_back();
+    dlst1.pop_back();
+    dlst1.print_dlst();
+    dlst1.erase(6);
+    dlst1.insert(1, 78.9);
+    dlst1.print_dlst();
+    dlst1.push_front(1.56);
+    dlst1.insert(0, 78.9);
+    dlst1.print_dlst();
+    dlst1.erase(50);
+    //cout << node;*/
+}
---
+++ DataStructures_Balakirev_Stepik/DoublyLinkedList/DoublyLinkedList_struct/DoublyLinkedList_struct.cpp
@@ -0,0 +1,261 @@
+﻿#include <iostream>
+using namespace std;
+
+// Описание задачи - внутри двусвязного списка работать со структурой GOODS
+// отсортировать элементы в порядке возрастания цены в двусвязном списке 
+// исхожный список goods оставить неизменным 
+
+typedef struct {
+    unsigned long id;	// идентификатор
+    char name[100];		// название
+    double price;		// цена
+} GOODS;
+
+class Node {
+public:
+    GOODS data;
+    Node* prev, * next;
+
+public:
+    Node(GOODS data) {
+        this->data = data;
+        this->prev = this->next = NULL;
+    }
+};
+
+class LinkedList {
+public:
+    Node* head, * tail;
+    int counter;
+
+public:
+    LinkedList() {
+        this->head = this->tail = NULL;
+        this->counter = 0;
+    }
+
+    LinkedList(const LinkedList& lst) { // конструктор копий 
+        this->head = lst.head;
+        this->tail = lst.tail;
+        this->counter = lst.counter;
+    }
+
+    ~LinkedList() {
+        while (head != NULL)
+            pop_front();
+    }
+
+    Node* push_front(GOODS data) {
+        Node* ptr = new Node(data);
+        ptr->next = head;
+        if (head != NULL)
+            head->prev = ptr;
+        if (tail == NULL)
+            tail = ptr;
+        head = ptr;
+        counter++;
+        return ptr;
+    }
+
+
+    Node* push_back(GOODS data) {
+        Node* ptr = new Node(data);
+
+        ptr->prev = tail;
+        if (tail != NULL)
+            tail->next = ptr;
+        if (head == NULL)
+            head = ptr;
+        tail = ptr;
+        counter++;
+        return ptr;
+    }
+
+    void pop_front() { // удаление из начала 
+        if (head == NULL) { // удалять нечего 
+            return;
+        }
+        Node* ptr = head->next;
+        if (ptr == NULL) { // элемент в списке только 1
+            delete head;
+            head = tail = NULL;
+            return;
+        }
+        ptr->prev = NULL;
+        delete head;
+        head = ptr;
+    }
+
+
+    void pop_back() {
+        if (tail == NULL) return;
+        Node* ptr = tail->prev;
+        if (ptr != NULL)
+            ptr->next = NULL;
+        else
+            head = NULL;
+        delete tail;
+        counter--;
+        tail = ptr;
+    }
+
+    Node* getAt(int index) {
+        Node* ptr = head;
+        int n = 0;
+        while (n != index) {
+            if (ptr == NULL)
+                return ptr;
+            ptr = ptr->next;
+            n++;
+        }
+        return ptr;
+    }
+
+
+    Node* operator [] (int index) {
+        return getAt(index);
+    }
+
+    Node* insert(int index, GOODS data) {
+        Node* right = getAt(index);
+        if (right == NULL)
+            return push_back(data);
+
+        Node* left = right->prev;
+        if (left == NULL)
+            return push_front(data);
+        Node* ptr = new Node(data);
+        ptr->prev = left;
+        ptr->next = right;
+        left->next = ptr;
+        right->prev = ptr;
+        counter++;
+        return ptr;
+    }
+
+    void erase(int index) {
+        Node* ptr = getAt(index);
+        if (ptr == NULL)
+            return;
+        if (ptr->prev == NULL) {
+            pop_front();
+            return;
+        }
+        if (ptr->next == NULL) {
+            pop_back();
+            return;
+        }
+        Node* left = ptr->prev;
+        Node* right = ptr->next;
+        left->next = right;
+        right->prev = left;
+        counter--;
+        delete ptr;
+    }
+
+};
+void print_lst(LinkedList& lst) {
+    for (int i = 0; lst[i] != NULL; ++i) {
+        cout << (lst[i]->data).id << ' ' << (lst[i]->data).name << ' ' << (lst[i]->data).price << endl;
+    }
+}
+
+// сортировка пузырьком для двусвязного списка по цене  
+void bubble_sorting_lst(LinkedList& sort_goods) {
+    int n = sort_goods.counter;
+    for (int i = 0; i < (n - 1); ++i) { // идем до предпоследнего элемента 
+        double mn = (sort_goods[i]->data).price;
+        int ind_mn = i;
+        for (int j = i + 1; j < n; ++j) {
+            if ((sort_goods[j]->data).price < mn) {
+                mn = (sort_goods[j]->data).price;
+                ind_mn = j;
+            }
+        }
+        if (i != ind_mn) { // минимум поменялся
+            Node* node_mn = sort_goods.getAt(ind_mn);
+            Node* tmp = sort_goods.getAt(i);
+            /*sort_goods.erase(i);
+            sort_goods.insert(i, node_mn->data);
+            sort_goods.erase(ind_mn);
+            sort_goods.insert(ind_mn, tmp->data);*/
+            Node* tmp_left = tmp->prev;
+            Node* tmp_right = tmp->next;
+            Node* left = node_mn->prev;
+            Node* right = node_mn->next;
+            // напишем случай двух рядом стоящих элементов
+            if (left == tmp) {
+                tmp->next = right;
+                tmp->prev = node_mn;
+                node_mn->next = tmp;
+                node_mn->prev = tmp_left;
+                if (tmp_left != NULL) {
+                    tmp_left->next = node_mn;
+                }
+                if (right != NULL) {
+                    right->prev = tmp;
+                }
+            }
+            // между ними только 1 элемент 
+            else if (tmp_right == left) {
+                tmp->next = right;
+                if (right != NULL) {
+                    right->prev = tmp;
+                }
+                tmp->prev = left;
+                left->next = tmp;
+                left->prev = node_mn;
+                node_mn->next = left;
+                node_mn->prev = tmp_left;
+                if (tmp_left != NULL) {
+                    tmp_left->next = node_mn;
+                }
+            }
+            else { // между ними хотя бы 2 элемента 
+                tmp->next = right;
+                tmp->prev = left;
+                if (right != NULL) {
+                    right->prev = tmp;
+                }
+                left->next = tmp;
+                node_mn->next = tmp_right;
+                node_mn->prev = tmp_left;
+                tmp_right->prev = node_mn;
+                if (tmp_left != NULL) {
+                    tmp_left->next = node_mn;
+                }
+            }
+
+        }
+    }
+}
+
+int main(void)
+{
+    setlocale(LC_ALL, "RUS");
+    GOODS g1 = {1, "стол", 5443.77 };
+    GOODS g2 = { 5, "зеркало", 12400.0 };
+    GOODS g3 = { 2, "кровать", 65000.0 };
+    GOODS g4 = { 8, "кресло", 34500.0 };
+    GOODS g5 = { 11, "стенка", 120400.0 };
+    GOODS g6 = { 3, "холодильник", 65500.0 };
+    LinkedList goods;
+    goods.push_back(g1);
+    goods.push_back(g2);
+    goods.push_back(g3);
+    goods.push_back(g4);
+    goods.push_back(g5);
+    goods.push_back(g6); 
+    cout << "before sorting" << endl;
+    print_lst(goods);
+    LinkedList sort_goods;
+    // заполняем список sort_goods
+    for (int i = 0; i < goods.counter; ++i) {
+        sort_goods.push_back(goods[i]->data);
+    }
+    bubble_sorting_lst(sort_goods);
+    cout << endl;
+    cout << "after sorting" << endl;
+    print_lst(sort_goods);
+    return 0;
+}
