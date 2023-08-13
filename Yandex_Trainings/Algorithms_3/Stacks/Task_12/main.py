1123df216d862bb520cbc8ed09afd6200e030a48
ark0797 <n0797@yandex.ru>
2023-08-13 18:46:47 +0300

    add Task 12 adv

---
+++ Yandex_Trainings/Algorithms_3/Stacks/Task_12/main.py
@@ -0,0 +1,23 @@
+# Determining the correct bracket sequence via using a stack
+
+s = input()
+stack = []
+for skobk in s:
+    if skobk == '(' or skobk == '[' or skobk == '{':
+        stack.append(skobk)
+    else: # встретили закрыв скобку
+        if len(stack) == 0:
+            print('no')
+            break
+        else: # длина стэка не 0
+            last_elem = stack[-1] # последний элемент
+            if (skobk == '}' and last_elem == '{') or (skobk == ')' and last_elem == '(') or (skobk == ']' and last_elem == '['):
+                stack.pop()
+            else:
+                print('no')
+                break
+else:
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
