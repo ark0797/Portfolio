340db4b5e1d78bfb2006540345f3a15e63f821c0
ark0797 <n0797@yandex.ru>
2023-08-14 17:51:14 +0300

    add stack with parenthesises

---
+++ DataStructures_Balakirev_Stepik/Deque_Stack_Queue/Stack_and_Parenthesises/main.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def get_sub_eq(eq_str):
+    st1 = deque()
+    res = []
+    st2 = deque()
+    for i, x in enumerate(eq_str):
+        if x == "(":
+            st1.append(i)
+        elif x == ")":
+            res.append(eq_str[st1.pop()+1: i])
+        elif x == '[':
+            st2.append(i)
+        elif x == ']':
+            res.append(eq_str[st2.pop() + 1: i])
+    return res
+
+
+s = "2 + 3 * (1 - 5 - [3 * (x) - 5]) + (a - b)"
+res = get_sub_eq(s)
+print(res)
