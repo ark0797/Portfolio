9f94986a514e3258aaccc5a58747e6e4d9914aad
ark0797 <n0797@yandex.ru>
2023-09-10 22:03:16 +0300

    add phone book

---
+++ Algorithms_Theory_and_Practice_CSC_Stepik/Hash_Tables/Phone_Book/Phone_Book.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+// Реализация простой структуры данных, что эффективно будет поддерживать запросы типа:
+// add number name - внести в таблицу пару (name, number)
+// del number - удалить запись по номеру телефона (если такого нет, то ничего не делать)
+// find number - найти имя name по номеру телефона (если такого номера нет, то not found) 
+// любой номер телефона содержит не более 7 цифр 
+
+// Практика реализации хэш-таблицы методом прямой адресации (самый простой способ на массиве) 
+
+
+int main()
+{
+    int n; // число запросов
+    cin >> n;
+    string oper, name;
+    int number;
+    vector<string> hash_table(10000000, ""); // хэш-таблица 
+    for (int i = 0; i < n; ++i) {
+        cin >> oper;
+        if (oper == "add") { // добавление пары 
+            cin >> number >> name;
+            hash_table[number] = name;
+        }
+        else if (oper == "del") { // удаление по номеру телефона 
+            cin >> number;
+            hash_table[number] = "";
+        }
+        else if (oper == "find") { // поиск имени по номеру 
+            cin >> number;
+            if (hash_table[number] == "") cout << "not found" << endl;
+            else {
+                cout << hash_table[number] << endl;
+            }
+        }
+    }
+    return 0;
+}
+
